8b8f8b0519138125e907b890e197cf08018dd28d
Ben Parish <parishben13@gmail.com>
2020-05-05 03:47:59 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+<<<<<<< HEAD
 def parrot(hey="Squawk!")
   puts "#{hey}"
   return "#{hey}"
+=======
+def parrot
+  puts "Squawk!"
+parrot
+>>>>>>> e3ee9524fb3ddebdcee99d8ed2b03a92374c8efa
 end
 
